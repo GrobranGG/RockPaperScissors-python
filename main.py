92567bedb296c602f12a24873ed38b59dc4331d5
Lev <grobran.gg@gmail.com>
2021-11-03 13:32:37 +0500

    RockPaperScissoors-ython v.1.1 update
    In this update I have optimized the code and added requirements.txt

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+# Rock, Paper, Scissors! v.1.1 (by GrobranGG) (https://github.com/GrobranGG)
 import random
 import time
 import os
 import colorama
+import sys
 
 from colorama import init
 from colorama import Fore, Back
@@ -11,111 +13,84 @@ os.system("clear")
 os.system("cls")
 
 print(Fore.GREEN + "Rock, Paper, Scissors!")
-print(Fore.RED + "v.1.0 (by GrobranGG)\n")
+print(Fore.RED + "v.1.1 (by GrobranGG)\n")
 
 print(Fore.WHITE + "Choose a language (number):")
 print("1. English")
 print("2. Russian")
 
-print(Back.RESET)
 print(Fore.CYAN)
 language = input("Enter your language: ")
 
+# Dictionary of all phrases in the code
+english = {'stone': 'Stone', 'scissors': 'Scissors', 'paper': 'Paper', 'youranswer': 'Your answer (number):', 'choose': 'Choose an item:',
+           'choosen_stone': 'You have chosen a stone!', 'choosen_scissors': 'You have chosen scissors!', 'choosen_paper': 'You have chosen paper!',
+           'computer_select': 'The computer selects the subject...', 'computer_select_stone': 'The computer chose a stone!',
+           'computer_select_scissors': 'The computer chose scissors!', 'computer_select_paper': 'The computer chose paper!', 'player_won': ' You`ve won!',
+           'player_lost': ' You`ve lost!', 'player_draw': ' You have a draw!'}
+
+russian = {'stone': 'Камень', 'scissors': 'Ножницы', 'paper': 'Бумага', 'youranswer': 'Твой ответ (число):', 'choose': 'Выбери предмет:',
+           'choosen_stone': 'Ты выбрал камень!', 'choosen_scissors': 'Ты выбрал ножницы!', 'choosen_paper': 'Ты выбрал бумагу!',
+           'computer_select': 'Компьютер выбирает предмет...', 'computer_select_stone': 'Компьютер выбрал камень!',
+           'computer_select_scissors': 'Компьютер выбрал ножницы!', 'computer_select_paper': 'Компьютер выбрал бумагу!', 'player_won': ' Ты выиграл!',
+           'player_lost': ' Ты проиграл!', 'player_draw': ' У вас ничья!'}
+
 if language == "1":
-    os.system("clear")
-    os.system("cls")
-    print(Fore.BLUE + "Choose an item:")
-    print(Fore.RESET + "1. Stone")
-    print("2. Scissors")
-    print("3. Paper\n")
-    gameobject = input("Your answer (number):")
-
-    if gameobject == "1":
-        print("You have chosen a stone!")
-        print("The computer selects the subject...")
-        time.sleep(2)
-        computergameobject = randint(1, 3)
-        if computergameobject == 1:
-            print(Fore.GREEN + "The computer chose a stone! You have a draw!")
-        elif computergameobject == 2:
-            print(Fore.GREEN + "The computer chose scissors! You've won!")
-        elif computergameobject == 3:
-            print(Fore.GREEN + "The computer chose paper! You've lost!")
-
-    elif gameobject == "2":
-        print("You have chosen scissors!")
-        print("The computer selects the subject...")
-        time.sleep(2)
-        computergameobject = randint(1, 3)
-        if computergameobject == 1:
-            print(Fore.GREEN + "The computer chose a stone! You've lost!")
-        elif computergameobject == 2:
-            print(Fore.GREEN + "The computer chose scissors! You have a draw!")
-        elif computergameobject == 3:
-            print(Fore.GREEN + "The computer chose paper! You've won!")
-
-    elif gameobject == "3":
-        print("You have chosen paper!")
-        print("The computer selects the subject...")
-        time.sleep(2)
-        computergameobject = randint(1, 3)
-        if computergameobject == 1:
-            print(Fore.GREEN + "The computer chose a stone! You've won!")
-        elif computergameobject == 2:
-            print(Fore.GREEN + "The computer chose scissors! You've lost!")
-        elif computergameobject == 3:
-            print(Fore.GREEN + "The computer chose paper! You have a draw!")
-    else:
-        print(Fore.RED + "Wrong choice!")
-
+    lang = english
 elif language == "2":
-    os.system("clear")
-    os.system("cls")
-    print(Fore.BLUE + "Выбери предмет:")
-    print(Fore.RESET + "1. Камень")
-    print("2. Ножницы")
-    print("3. Бумага\n")
-    gameobject = input("Ваш ответ (число):")
-
-    if gameobject == "1":
-        print("Вы выбрали камень!")
-        print("Компьютер выбирает предмет...")
-        time.sleep(2)
-        computergameobject = randint(1, 3)
-        if computergameobject == 1:
-            print(Fore.GREEN + "Компьютер выбрал камень! У вас ничья!")
-        elif computergameobject == 2:
-            print(Fore.GREEN + "Компьютер выбрал ножницы! Вы выйграли!")
-        elif computergameobject == 3:
-            print(Fore.GREEN + "Компьютер выбрал бумагу! Вы проиграли!")
-
-    elif gameobject == "2":
-        print("Вы выбрали ножницы!")
-        print("Компьютер выбирает предмет...")
-        time.sleep(2)
-        computergameobject = randint(1, 3)
-        if computergameobject == 1:
-            print(Fore.GREEN + "Компьютер выбрал камень! Вы проиграли!")
-        elif computergameobject == 2:
-            print(Fore.GREEN + "Компьютер выбрал ножницы! У вас ничья!")
-        elif computergameobject == 3:
-            print(Fore.GREEN + "Компьютер выбрал бумагу! Вы выйграли!")
-
-    elif gameobject == "3":
-        print("Вы выбрали бумагу!")
-        print("Компьютер выбирает предмет...")
-        time.sleep(2)
-        computergameobject = randint(1, 3)
-        if computergameobject == 1:
-            print(Fore.GREEN + "Компьютер выбрал камень! Вы выйграли!")
-        elif computergameobject == 2:
-            print(Fore.GREEN + "Компьютер выбрал ножницы! Вы проиграли!")
-        elif computergameobject == 3:
-            print(Fore.GREEN + "Компьютер выбрал бумагу! У вас ничья!")
-    else:
-        print(Fore.RED + "Неверный выбор!")
-
+    lang = russian
 else:
     print(Fore.RED + "Incorrect language!")
+    input()
+    sys.exit()
+
+print(Back.RESET)
+
+os.system("clear")
+os.system("cls")
+print(Fore.BLUE + lang['choose'])
+print(Fore.RESET + "1." + lang['stone'])
+print("2." + lang['scissors'])
+print("3." + lang['paper'] + "\n")
+gameobject = input(lang['youranswer'])
+if gameobject == "1":
+    print(lang['choosen_stone'])
+    print(lang['computer_select'])
+    time.sleep(2)
+    computergameobject = randint(1, 3)
+    if computergameobject == 1:
+        print(Fore.GREEN + lang['computer_select_stone'] + lang['player_draw'])
+    elif computergameobject == 2:
+        print(Fore.GREEN + lang['computer_select_scissors'] + lang['player_won'])
+    elif computergameobject == 3:
+        print(Fore.GREEN + lang['computer_select_paper'] + lang['player_lost'])
+
+elif gameobject == "2":
+    print(lang['choosen_scissors'])
+    print(lang['computer_select'])
+    time.sleep(2)
+    computergameobject = randint(1, 3)
+    if computergameobject == 1:
+        print(Fore.GREEN + lang['computer_select_stone'] + lang['player_lost'])
+    elif computergameobject == 2:
+        print(Fore.GREEN + lang['computer_select_scissors'] + lang['player_draw'])
+    elif computergameobject == 3:
+        print(Fore.GREEN + lang['computer_select_paper'] + lang['player_won'])
+
+elif gameobject == "3":
+    print(lang['choosen_paper'])
+    print(lang['computer_select'])
+    time.sleep(2)
+    computergameobject = randint(1, 3)
+    if computergameobject == 1:
+        print(Fore.GREEN + lang['computer_select_stone'] + lang['player_won'])
+    elif computergameobject == 2:
+        print(Fore.GREEN + lang['computer_select_scissors'] + lang['player_lost'])
+    elif computergameobject == 3:
+        print(Fore.GREEN + lang['computer_select_paper'] + lang['player_draw'])
+else:
+    print(Fore.RED + "Wrong choice!")
+    input()
+    sys.exit()
 
 input()
