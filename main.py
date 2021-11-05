e96ae5aa63e235636e7d34e98931e76cb28da3c0
Lev <grobran.gg@gmail.com>
2021-11-05 16:20:56 +0500

    RockPaperScissors-python v.1.2 update
    In this update, I added an action selection at the end and slightly changed the appearance of the application.

--- main.py
+++ main.py
@@ -1,102 +1,123 @@
-# Rock, Paper, Scissors! v.1.1 (by GrobranGG) (https://github.com/GrobranGG)
+# Rock, Paper, Scissors! v.1.2 (by GrobranGG) (https://github.com/GrobranGG)
 import random
 import time
 import os
 import colorama
 import sys
 
-opersystem = sys.platform
-from colorama import Fore, Back
-from random import randint
-colorama.init()
+def convert():
+    from colorama import Fore, Back
+    from random import randint
+    colorama.init()
 
-if opersystem == "win32":
-    os.system("cls")
-elif opersystem == 'linux':
-    os.system("clear")
+    opersystem = sys.platform
+    if opersystem == "win32":
+        os.system("cls")
+    elif opersystem == 'linux':
+        os.system("clear")
 
-print(Fore.GREEN + "Rock, Paper, Scissors!")
-print(Fore.RED + "v.1.1 (by GrobranGG)\n")
+    print(Fore.GREEN)
+    print(" _____            _      _____                       _____      _                         ")
+    print("|  __ \          | |    |  __ \                     / ____|    (_)                        ")
+    print("| |__) |___   ___| | __ | |__) |_ _ _ __   ___ _ __| (___   ___ _ ___ ___  ___  _ __ ___  ")
+    print("|  _  // _ \ / __| |/ / |  ___/ _` | '_ \ / _ \ '__|\___ \ / __| / __/ __|/ _ \| '__/ __| ")
+    print("| | \ \ (_) | (__|   < _| |  | (_| | |_) |  __/ | _ ____) | (__| \__ \__ \ (_) | |  \__ \ ")
+    print("|_|  \_\___/ \___|_|\_( )_|   \__,_| .__/ \___|_|( )_____/ \___|_|___/___/\___/|_|  |___/")
+    print("                      |/           | |           |/                                      ")
+    print("                                   |_|                                                   ")
+    print(Fore.RED + "                                                                      v.1.2 (by GrobranGG)\n")
 
-print(Fore.WHITE + "Choose a language (number):")
-print("1. English")
-print("2. Russian")
+    print(Fore.WHITE + "Choose a language (number):")
+    print("1. English")
+    print("2. Russian")
 
-print(Fore.CYAN)
-language = input("Enter your language: ")
+    print(Fore.CYAN)
+    language = input("Enter your language: ")
 
-# Dictionary of all phrases in the code
-english = {'stone': 'Stone', 'scissors': 'Scissors', 'paper': 'Paper', 'youranswer': 'Your answer (number):', 'choose': 'Choose an item:',
-           'choosen_stone': 'You have chosen a stone!', 'choosen_scissors': 'You have chosen scissors!', 'choosen_paper': 'You have chosen paper!',
-           'computer_select': 'The computer selects the subject...', 'computer_select_stone': 'The computer chose a stone!',
-           'computer_select_scissors': 'The computer chose scissors!', 'computer_select_paper': 'The computer chose paper!', 'player_won': ' You`ve won!',
-           'player_lost': ' You`ve lost!', 'player_draw': ' You have a draw!'}
+    # Dictionary of all phrases in the code
+    english = {'stone': 'Stone', 'scissors': 'Scissors', 'paper': 'Paper', 'youranswer': 'Your answer (number):', 'choose': 'Choose an item:',
+               'choosen_stone': 'You have chosen a stone!', 'choosen_scissors': 'You have chosen scissors!', 'choosen_paper': 'You have chosen paper!',
+               'computer_select': 'The computer selects the subject...', 'computer_select_stone': 'The computer chose a stone!',
+               'computer_select_scissors': 'The computer chose scissors!', 'computer_select_paper': 'The computer chose paper!', 'player_won': ' You`ve won!',
+               'player_lost': ' You`ve lost!', 'player_draw': ' You have a draw!', 'repeat_question': 'Choose the next action:', 'repeat_variant1': '1. Restart the program',
+               'repeat_variant2': '2. Close the program', 'answer_repeat': 'Choose the answer (number): '}
 
-russian = {'stone': 'Камень', 'scissors': 'Ножницы', 'paper': 'Бумага', 'youranswer': 'Твой ответ (число):', 'choose': 'Выбери предмет:',
-           'choosen_stone': 'Ты выбрал камень!', 'choosen_scissors': 'Ты выбрал ножницы!', 'choosen_paper': 'Ты выбрал бумагу!',
-           'computer_select': 'Компьютер выбирает предмет...', 'computer_select_stone': 'Компьютер выбрал камень!',
-           'computer_select_scissors': 'Компьютер выбрал ножницы!', 'computer_select_paper': 'Компьютер выбрал бумагу!', 'player_won': ' Ты выиграл!',
-           'player_lost': ' Ты проиграл!', 'player_draw': ' У вас ничья!'}
+    russian = {'stone': 'Камень', 'scissors': 'Ножницы', 'paper': 'Бумага', 'youranswer': 'Твой ответ (число):', 'choose': 'Выбери предмет:',
+               'choosen_stone': 'Ты выбрал камень!', 'choosen_scissors': 'Ты выбрал ножницы!', 'choosen_paper': 'Ты выбрал бумагу!',
+               'computer_select': 'Компьютер выбирает предмет...', 'computer_select_stone': 'Компьютер выбрал камень!',
+               'computer_select_scissors': 'Компьютер выбрал ножницы!', 'computer_select_paper': 'Компьютер выбрал бумагу!', 'player_won': ' Ты выиграл!',
+               'player_lost': ' Ты проиграл!', 'player_draw': ' У вас ничья!', 'repeat_question': 'Выберите следующее действие:', 'repeat_variant1': '1. Переапустить программу',
+               'repeat_variant2': '2. Закрыть программу', 'answer_repeat': 'Выбери ответ (число): '}
 
-if language == "1":
-    lang = english
-elif language == "2":
-    lang = russian
-else:
-    print(Fore.RED + "Incorrect language!")
-    input()
-    sys.exit()
+    if language == "1":
+        lang = english
+    elif language == "2":
+        lang = russian
+    else:
+        print(Fore.RED + "Incorrect language!")
+        input()
+        sys.exit()
 
-print(Back.RESET)
+    print(Back.RESET)
 
-if opersystem == "win32":
-    os.system("cls")
-elif opersystem == 'linux':
-    os.system("clear")
+    if opersystem == "win32":
+        os.system("cls")
+    elif opersystem == 'linux':
+        os.system("clear")
 
-print(Fore.BLUE + lang['choose'])
-print(Fore.RESET + "1." + lang['stone'])
-print("2." + lang['scissors'])
-print("3." + lang['paper'] + "\n")
-gameobject = input(lang['youranswer'])
-if gameobject == "1":
-    print(lang['choosen_stone'])
-    print(lang['computer_select'])
-    time.sleep(2)
-    computergameobject = randint(1, 3)
-    if computergameobject == 1:
-        print(Fore.GREEN + lang['computer_select_stone'] + lang['player_draw'])
-    elif computergameobject == 2:
-        print(Fore.GREEN + lang['computer_select_scissors'] + lang['player_won'])
-    elif computergameobject == 3:
-        print(Fore.GREEN + lang['computer_select_paper'] + lang['player_lost'])
+    print(Fore.BLUE + lang['choose'])
+    print(Fore.RESET + "1." + lang['stone'])
+    print("2." + lang['scissors'])
+    print("3." + lang['paper'] + "\n")
+    gameobject = input(lang['youranswer'])
+    if gameobject == "1":
+        print(lang['choosen_stone'])
+        print(lang['computer_select'])
+        time.sleep(2)
+        computergameobject = randint(1, 3)
+        if computergameobject == 1:
+            print(Fore.GREEN + lang['computer_select_stone'] + lang['player_draw'])
+        elif computergameobject == 2:
+            print(Fore.GREEN + lang['computer_select_scissors'] + lang['player_won'])
+        elif computergameobject == 3:
+            print(Fore.GREEN + lang['computer_select_paper'] + lang['player_lost'])
 
-elif gameobject == "2":
-    print(lang['choosen_scissors'])
-    print(lang['computer_select'])
-    time.sleep(2)
-    computergameobject = randint(1, 3)
-    if computergameobject == 1:
-        print(Fore.GREEN + lang['computer_select_stone'] + lang['player_lost'])
-    elif computergameobject == 2:
-        print(Fore.GREEN + lang['computer_select_scissors'] + lang['player_draw'])
-    elif computergameobject == 3:
-        print(Fore.GREEN + lang['computer_select_paper'] + lang['player_won'])
+    elif gameobject == "2":
+        print(lang['choosen_scissors'])
+        print(lang['computer_select'])
+        time.sleep(2)
+        computergameobject = randint(1, 3)
+        if computergameobject == 1:
+            print(Fore.GREEN + lang['computer_select_stone'] + lang['player_lost'])
+        elif computergameobject == 2:
+            print(Fore.GREEN + lang['computer_select_scissors'] + lang['player_draw'])
+        elif computergameobject == 3:
+            print(Fore.GREEN + lang['computer_select_paper'] + lang['player_won'])
 
-elif gameobject == "3":
-    print(lang['choosen_paper'])
-    print(lang['computer_select'])
-    time.sleep(2)
-    computergameobject = randint(1, 3)
-    if computergameobject == 1:
-        print(Fore.GREEN + lang['computer_select_stone'] + lang['player_won'])
-    elif computergameobject == 2:
-        print(Fore.GREEN + lang['computer_select_scissors'] + lang['player_lost'])
-    elif computergameobject == 3:
-        print(Fore.GREEN + lang['computer_select_paper'] + lang['player_draw'])
-else:
-    print(Fore.RED + "Wrong choice!")
-    input()
-    sys.exit()
+    elif gameobject == "3":
+        print(lang['choosen_paper'])
+        print(lang['computer_select'])
+        time.sleep(2)
+        computergameobject = randint(1, 3)
+        if computergameobject == 1:
+            print(Fore.GREEN + lang['computer_select_stone'] + lang['player_won'])
+        elif computergameobject == 2:
+            print(Fore.GREEN + lang['computer_select_scissors'] + lang['player_lost'])
+        elif computergameobject == 3:
+            print(Fore.GREEN + lang['computer_select_paper'] + lang['player_draw'])
+    else:
+        print(Fore.RED + "Wrong choice!")
+        input()
+        sys.exit()
 
-input()
+    print("\n")
+    print(Fore.CYAN + lang['repeat_question'])
+    print(Fore.GREEN + lang['repeat_variant1'])
+    print(Fore.GREEN + lang['repeat_variant2'])
+    repeat = int(input(Fore.WHITE + lang['answer_repeat']))
+
+    if repeat == 1:
+        convert()
+    if repeat == 2:
+        sys.exit()
+convert()
